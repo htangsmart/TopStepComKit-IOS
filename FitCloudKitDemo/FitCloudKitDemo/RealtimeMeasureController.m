f2d02f6d2bd74479b37a20b5bd7fc2d65fff72d3
dz <535052789@qq.com>
2024-06-17 13:56:44 +0800

    增加联系人相关功能

--- FitCloudKitDemo/FitCloudKitDemo/RealtimeMeasureController.m
+++ FitCloudKitDemo/FitCloudKitDemo/RealtimeMeasureController.m
@@ -23,28 +23,17 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    
+    if (indexPath.row == 0) {
+        
+    } else if (indexPath.row == 1) {
+        
+    }
 }
 
-/*
- #pragma mark - Navigation
- 
- // In a storyboard-based application, you will often want to do a little preparation before navigation
- - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
- // Get the new view controller using [segue destinationViewController].
- // Pass the selected object to the new view controller.
- }
- */
 - (IBAction)OnGoBack:(id)sender {
     [self.navigationController popViewControllerAnimated:YES];
 }
